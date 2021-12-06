3c40cd3d53616ab5b000904483cbfd75c6dd2395
chanon-mike <s1f102100041@iniad.org>
2021-12-06 18:56:11 +0700

    Design the webpage

--- train_delay/views.py
+++ train_delay/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from train_delay.models import TrainInfo
 
 # Create your views here.
 def index(request):
-    return render(request, 'train_delay/index.html')
+    train_info =  TrainInfo.objects.all()
+    data = {
+        'information': train_info,
+        'operator_list': set([info.operator for info in train_info])
+    }
+    return render(request, 'train_delay/index.html', data)
