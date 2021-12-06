0cc26fd0f7eae5b7b8f568d95d6593ee1a74872b
YadanarLin <s1F102100030@iniad.org>
2021-12-06 18:37:15 +0630

    views.py

--- line_bot/views.py
+++ line_bot/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from utils import message_creator
+from line_bot.line_message import lineMessage
+
+@csrf_exempt
+def index(request):
+    if request.method=='POST':
+        request = json.loads(request.body.decode('utf-8'))
+        events=request['events']
+        for event in events:
+            message=event['message']
+            reply_token=event['replyToken']
+            line_message=lineMessage(message_creator.create_single_text_message(message['text']))
+            line_message.reply(reply_token)
+        return HttpResponse('ok')
