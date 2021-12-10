22254e5bac375e4eec17674005ad0d17f73ccb87
chanon-mike <s1f102100041@iniad.org>
2021-12-10 12:20:23 +0700

    Update webpage

--- train_delay/urls.py
+++ train_delay/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'train_delay'
 urlpatterns = [
     path('', views.index, name="index"),
+    path('<int:id>/detail', views.detail, name="detail"),
 ]
--- train_delay/views.py
+++ train_delay/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from train_delay.models import TrainInfo
+from train_delay.database_util import check_last_update
 
 # Create your views here.
 def index(request):
-    train_info =  TrainInfo.objects.all()
+    check_last_update()
+    train_info =  TrainInfo.objects.order_by('operator')
     data = {
         'information': train_info,
-        'operator_list': set([info.operator for info in train_info])
+        'operator_list': sorted(list(set([info.operator for info in train_info])))
     }
+    # from pprint import pprint
+    # for info in train_info:
+    #     pprint(info.operator)
+    # print(data['operator_list'])
+
     return render(request, 'train_delay/index.html', data)
+
+
+def detail(request, id):
+    context = {
+        'information': TrainInfo.objects.get(pk=id)
+    }
+    
+    return render(request, 'train_delay/detail.html', context)
