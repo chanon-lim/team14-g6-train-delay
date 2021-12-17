7a867581aecf0ccc2701a9ae72de353f3288a71b
Tung <s1f102100029@iniad.org>
2021-12-17 19:56:44 +0700

    clean git

--- test.py
+++
@@ -1,9 +0,0 @@
-from config import wsgi
-from train_delay.database_util import refresh_database,check_last_update
-# from test import get_all_operator
-# from test import get_all_line_all_operator
-# from test import get_all_line_information
-# from twitter_bot.worker import test_read_json
-
-check_last_update()
-
